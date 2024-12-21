6187e1857b42eebeaa1b61116eca9381a893b230
allyelvis <allyelvis6569@gmail.com>
2024-12-21 22:44:13 +0000

    initilize

---
+++ resources/js/components/BookingCalendar.vue
@@ -0,0 +1,14 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<script setup>
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+const calendarOptions = {
+  plugins: [dayGridPlugin],
+  initialView: 'dayGridMonth',
+  events: '/api/bookings'
+};
+</script>
